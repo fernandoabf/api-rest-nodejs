21a64ef22e296f3c53f195baa67b5386bd2cfb5c
Fernando <capcakeplay@Hotmail.com>
2024-09-06 14:37:23 -0300

    Render Details

--- src/env/index.ts
+++ src/env/index.ts
@@ -11,7 +11,7 @@ const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'production', 'test']).default('production'),
   DATABASE_URL: z.string(),
   DATABASE_CLIENT: z.enum(['sqlite', 'pg']),
-  PORT: z.coerce.number().default(3333),
+  PORT: z.coerce.number().default(4000),
 });
 
 export const _env = envSchema.safeParse(process.env);
